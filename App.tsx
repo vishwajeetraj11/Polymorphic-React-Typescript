c59b3d50b8585c9f401313bf37a87e90fd99e457
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2022-06-16 18:57:16 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -3,25 +3,30 @@ import Typography from './components/Typography';
 import './style.css';
 
 export default function App() {
-  const ref = React.useRef<HTMLAnchorElement | null>(null);
-  const ref2 = React.useRef<HTMLHeadingElement | null>(null);
+  const ref2 = React.useRef<HTMLAnchorElement | null>(null);
+  const ref = React.useRef<HTMLHeadingElement | null>(null);
+
+  React.useEffect(() => {
+    console.log(ref);
+  }, [ref]);
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <Typography
-        ref={ref2}
+        ref={ref}
         as="h1"
         color="indigo"
         style={{ backgroundColor: 'black' }}
       >
         Start editing to see some magic happen :)
       </Typography>
-      <Typography as={Emphasis}>
-        Start editing to see some magic happen :)
-      </Typography>
-      <Typography style={{}}>
-        Start editing to see some magic happen :)
-      </Typography>
+      {/* <Typography as={Emphasis}>
+          Start editing to see some magic happen :)
+        </Typography>
+        <Typography style={{}}>
+          Start editing to see some magic happen :)
+        </Typography> */}
     </div>
   );
 }
